f291cf8f1c4cbb65aab95a3d4a74b0f747e74255
Christoph <lernschnitzel@gmail.com>
2023-09-21 15:27:06 +0200

    Fixed stamp embossing issue. CSS fixes

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,4 @@
 <script setup lang="ts">
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
 import StempelView from './components/StempelView.vue'
 import { ref } from 'vue'
 const count = ref("Stempel")
@@ -12,48 +10,53 @@ const blur = ref(0)
 
 <template>
   <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
 
-    <div class="wrapper">
-      <HelloWorld :msg="count as unknown as number" />
-    </div>
   </header>
 
-  <main class="container">
-    <div class="mb-2">
-      <label for="large-input" class="block mb-2 text-sm font-medium
+  <main>
+    <div class="container mx:auto overflow-x-hidden">
+      <div class="mb-2">
+        <label for="large-input" class="block mb-2 text-sm font-medium
     text-gray-900 dark:text-white">Stempeltext (Maximal 24 Zeichen)</label><br>
-      <input type="text" id="large-input" size="24" maxlength="24" class="block w-full p-4
+        <input type="text" id="large-input" size="24" maxlength="24" class="block w-full p-4
     text-gray-900
     border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500
     focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600
     dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500
     dark:focus:border-blue-500" v-model="count as string">
-      <label for="steps-range"
-        class="w-screen block my-5 text-sm font-medium text-gray-900 dark:text-white">Stamp height</label>
-      <div class="grid grid-flow-row-dense grid-cols-5 gap-4 mb-5">
-        <input id="steps-range" type="range" min="0" max="5" v-model="slider as number" step="0.5" class="col-span-4 h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer
+        <label for="steps-range" class="w-screen block my-5 text-sm font-medium
+      text-gray-900 dark:text-white">Stamp
+          height</label>
+        <div class="grid grid-flow-row-dense grid-cols-5 gap-4 mb-5">
+          <input id="steps-range" type="range" min="0" max="5" v-model="slider as
+            number" step="0.5" class="col-span-4 h-2 bg-gray-200 rounded-lg
+        appearance-none cursor-pointer
 dark:bg-gray-700">
-        <label class="block text-lg align-middle font-medium text-gray-900 dark:text-white">{{ slider }}</label>
-      </div>
-      <label for="height-range"
-        class="w-screen block my-5 text-sm font-medium text-gray-900 dark:text-white">Embossing height</label>
-      <div class="grid grid-flow-row-dense grid-cols-5 gap-4 mb-5">
-        <input id="height-range" type="range" min="0" max="1" v-model="height as number" step="0.05" class="col-span-4 h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer
+          <label class="block text-lg align-middle font-medium text-gray-900
+        dark:text-white text-center">{{ slider }}</label>
+        </div>
+        <label for="height-range" class="w-screen block my-5 text-sm font-medium
+      text-gray-900 dark:text-white">Embossing
+          height</label>
+        <div class="grid grid-flow-row-dense grid-cols-5 gap-4 mb-5">
+          <input id="height-range" type="range" min="0" max="1" v-model="height as number" step="0.05" class="col-span-4 h-2
+        bg-gray-200 rounded-lg appearance-none cursor-pointer 
 dark:bg-gray-700">
-        <label class="block text-lg align-middle font-medium text-gray-900
-dark:text-white">{{ height }}</label>
-      </div>
-      <label for="blur-range" class="w-screen block my-5 text-sm font-medium
+          <label class="block text-lg align-middle font-medium text-gray-900
+dark:text-white  text-center ">{{ height }}</label>
+        </div>
+        <label for="blur-range" class="w-screen block my-5 text-sm font-medium
 text-gray-900 dark:text-white">Blur</label>
-      <div class="grid grid-flow-row-dense grid-cols-5 gap-4 mb-5">
-        <input id="blur-range" type="range" min="0" max="8" v-model="blur as number" step="1" class="col-span-4 h-2 bg-gray-200 rounded-lg appearance-none cursor-pointer
+        <div class="grid grid-flow-row-dense grid-cols-5 gap-4 mb-5">
+          <input id="blur-range" type="range" min="0" max="8" v-model="blur as number" step="1" class="col-span-4 h-2 bg-gray-200 rounded-lg
+          appearance-none cursor-pointer
 dark:bg-gray-700">
-        <label class="block text-lg align-middle font-medium text-gray-900
-dark:text-white">{{ blur }}</label>
+          <label class="block text-lg align-middle font-medium text-gray-900
+dark:text-white  text-center">{{ blur }}</label>
+        </div>
       </div>
+      <StempelView :msg="count as string" :height="height as number" :padding="slider as number" :blurryness="blur" />
     </div>
-    <StempelView :msg="count as string" :height="height as number" :padding="slider as number" :blurryness="blur" />
   </main>
 </template>
 
--- src/components/StempelView.vue
+++ src/components/StempelView.vue
@@ -1,8 +1,7 @@
 <script setup lang="ts">
 import * as THREE from 'three';
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-import { STLExporter } from "three/examples/jsm/exporters/STLExporter";
-import { mergeBufferGeometries } from 'three/examples/jsm/utils/BufferGeometryUtils';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+import { STLExporter } from "three/examples/jsm/exporters/STLExporter.js";
 import { redrawCanvas } from '../utils'
 const props = defineProps<{
   msg: string
@@ -11,7 +10,6 @@ const props = defineProps<{
   blurryness: number
 }>()
 import { onMounted, onUpdated } from 'vue'
-import { add } from 'three/examples/jsm/nodes/Nodes.js';
 let stempel: THREE.Mesh<any, THREE.MeshBasicMaterial, THREE.Object3DEventMap>;
 let body: THREE.Mesh<any, THREE.MeshStandardMaterial, THREE.Object3DEventMap>;
 let sceneObject: THREE.Scene
@@ -19,6 +17,7 @@ let parametersUpdated = false;
 let currentTexture: THREE.CanvasTexture
 let aspectRatioX = 4
 let aspectRatioY = 1
+let DEBUG = false;
 
 function onDownloadButtonPressed() {
   let copy = stempel.clone()
@@ -31,9 +30,6 @@ function onDownloadButtonPressed() {
   const canvasWidth = canvas.width;
   const canvasHeight = canvas.height;
 
-
-
-  // Get the entire canvas pixel data
   const pixelData = context.getImageData(0, 0, canvasWidth, canvasHeight).data;
 
   const heightmapData = pixelData // Assuming heightmap is grayscale
@@ -50,18 +46,14 @@ function onDownloadButtonPressed() {
     const yn = (aspectRatioY / 2 - y) / aspectRatioY * (canvasHeight - 1); // Adjust for your mesh size
     const pixelIndex = (Math.floor(yn) * canvasWidth + Math.floor(xn)) * 4;
 
-    let displacement = heightmapData[pixelIndex] / 255 * -props.height; // Adjust maxHeight as needed
-    // console.log(pixelIndex, displacement, heightmapData[pixelIndex])
-    if (displacement < -props.height * 0.7) displacement = -props.height;
+    let displacement = heightmapData[pixelIndex] / 255 * -props.height;
+
+    // To enshure enough contact to paper. 
+    if (Math.abs(displacement) < (props.height * 0.85)) displacement = 0;
+
     vertices[i + 2] = displacement;
   }
 
-  //   const mergedGeometry = mergeBufferGeometries([copy.geometry, body.geometry]);
-  // const mergedMesh = new THREE.Mesh(mergedGeometry, copy.material); // You can use the material from one of the original meshes
-  // sceneObject.remove(body)
-  // sceneObject.remove(stempel)
-  // sceneObject.add(mergedMesh)
-  // copy.geometry.attributes.position.needsUpdate = true;
   const exporter = new STLExporter();
   const stlData = exporter.parse(sceneObject);
   const stlBlob = new Blob([stlData], { type: 'application/octet-stream' });
@@ -74,7 +66,7 @@ function onDownloadButtonPressed() {
   document.body.appendChild(downloadLink);
 
   // To trigger the download automatically, you can do:
-  //downloadLink.click();
+  if(!DEBUG) downloadLink.click();
 
 }
 
@@ -93,17 +85,19 @@ onMounted(() => {
   const camera = new THREE.PerspectiveCamera(75, window.innerWidth /
     window.innerHeight, 0.1, 1000);
   const renderer = new THREE.WebGLRenderer({ antialias: true });
-  renderer.setSize(window.innerWidth * 0.8, window.innerHeight * 0.8);
+  renderer.setSize(window.innerWidth * 0.8 , window.innerHeight * 0.8);
   renderer.setClearColor(0xe6f7eb)
   const app = document.querySelector<HTMLDivElement>("#canvas");
   app?.appendChild(renderer.domElement);
 
-  const axesHelper = new THREE.AxesHelper(10); // Specify the size of the axes (1 unit in this case)
-  scene.add(axesHelper);
+  if (DEBUG) {
+    const axesHelper = new THREE.AxesHelper(10); // Specify the size of the axes (1 unit in this case)
+    scene.add(axesHelper);
+  }
   const controls = new OrbitControls(camera, renderer.domElement);
 
   let addNewBody = (size: number) => {
-    const stampBodyMesh = new THREE.BoxGeometry(aspectRatioX, aspectRatioY, size*2);
+    const stampBodyMesh = new THREE.BoxGeometry(aspectRatioX, aspectRatioY, size * 2);
     const stampBodyMaterial = new THREE.MeshStandardMaterial({ color: 0xffffff });
     const stampBody = new THREE.Mesh(stampBodyMesh, stampBodyMaterial)
     stampBody.position.set(0, 0, -size)
@@ -161,10 +155,6 @@ onMounted(() => {
         stempel.material = material
       }
       stampFace.position.set(0, 0, props.height)
-      // let newZPosition = -props.padding / 2
-      // console.log(newZPosition)
-      // stampBody.scale.set(1, 1, stampBody.scale.z / props.padding)
-      // stampBody.position.set(0, 0, newZPosition)
       addNewBody((props.padding as number) / 2)
       parametersUpdated = false
     }
